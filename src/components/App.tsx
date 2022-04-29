39cb50fb623fafe451a831be90662b2da6472ae6
Jianan Sun <isunjn@gmail.com>
2022-04-29 13:46:20 +0800

    chore: set up

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import logo from "~/assets/svg/logo.svg";
+
+function App() {
+  return (
+    <div className="mx-auto w-fit text-blue-400">
+      <img src={logo} alt="logo" className="w-6 inline-block" />
+      pianoo
+    </div>
+  );
+}
+
+export default App;
