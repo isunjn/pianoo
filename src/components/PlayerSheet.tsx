3eb34c6d9329195837fbc4ff3bcabd8777e4b595
Jianan Sun <isunjn@gmail.com>
2022-07-06 14:28:27 +0800

    feat: zoom sheet hint text when hover

--- src/components/PlayerSheet.tsx
+++ src/components/PlayerSheet.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef, forwardRef, type Ref } from "react";
+import { HiCursorClick } from "react-icons/hi";
 import * as Tone from "tone";
 import Sheet from "~/components/Sheet";
 import type { PlayerState } from "~/components/Player";
@@ -38,12 +39,15 @@ const PlayerSheet = forwardRef(
 
     // TODO: handle overflow-x
     return (
-      <div ref={sheetContainerRef} className="w-full h-52 font-mono text-xl scrollbar-hidden">
+      <div ref={sheetContainerRef} className="w-full h-52 font-mono text-xl scrollbar-hidden group">
         <Sheet sheetItems={sheetItems} ref={ref} />
         {(state == "ready" || state == "paused") &&
           <div className="absolute top-10 left-0 w-full h-52 flex items-center justify-center
                         bg-[#7b9c98]/30 backdrop-blur-sm pointer-events-none">
-            {state == "ready" ? "Click or press any key to start" : "Pasued, click or press any key to resume"}
+            <div className="flex items-center gap-4 group-hover:scale-[1.025] transition-transform ease-in">
+              {state == "ready" ? "Click or press any key to start" : "Pasued, click or press any key to resume"} 
+              <HiCursorClick />
+            </div>
           </div>
         }
       </div>
