68ec582a70da10ca92045afcbc4637cbfe5f851b
Jianan Sun <isunjn@gmail.com>
2022-07-11 17:59:30 +0800

    chore: a few example scores

--- src/examples/index.ts
+++ src/examples/index.ts
@@ -1,9 +1,9 @@
 /* a few example music scores */
 
-import example001 from  "./001.json";
-import example002 from  "./002.json";
-
-export {
-  example001,
-  example002,
-}
+export { default as score001 } from "./001.json";
+export { default as score002 } from "./002.json";
+export { default as score003 } from "./003.json";
+export { default as score004 } from "./004.json";
+export { default as score005 } from "./005.json";
+export { default as score006 } from "./006.json";
+export { default as score007 } from "./007.json";
