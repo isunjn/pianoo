4adde5a9d463b5a97dde5a5b8a68402b94c53c72
Jianan Sun <isunjn@gmail.com>
2022-07-19 18:03:30 +0800

    chore: update example scores

--- src/examples/index.ts
+++ src/examples/index.ts
@@ -7,3 +7,7 @@ export { default as score004 } from "./004.json";
 export { default as score005 } from "./005.json";
 export { default as score006 } from "./006.json";
 export { default as score007 } from "./007.json";
+export { default as score008 } from "./008.json";
+export { default as score009 } from "./009.json";
+export { default as score010 } from "./010.json";
+export { default as score011 } from "./011.json";
